9ff71f0997eaadc36418d7418597e67929ded0c1
Workplace <avinashnishadpro@gmail.com>
2024-01-02 16:52:50 +0530

    Please save it.

---
+++ script1.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var colorChangeBtn = document.getElementById("colorChangeBtn");
+
+    colorChangeBtn.addEventListener("click", function () {
+        changeBackgroundColor();
+    });
+
+    function changeBackgroundColor() {
+        var randomColor = getRandomColor();
+        document.body.style.backgroundColor = randomColor;
+    }
+
+    function getRandomColor() {
+        var letters = "0123456789ABCDEF";
+        var color = "#";
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
